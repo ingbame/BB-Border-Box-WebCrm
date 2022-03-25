c825384f2a778d745433707345235a3fbda861b8
ingbame <ingbame@gmail.com>
2022-03-25 17:23:05 -0600

    Carga Inicial para crear Plantilla

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/myproject'),
+      dir: require('path').join(__dirname, './coverage/bb-border-box-web-crm'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-import { FullComponent } from './layouts/full/full.component';
+const routes: Routes = [];
 
-//FORMA DE ROUTEO "Lazy Loading"
-export const Approutes: Routes = [
-  {
-    path: '',
-    component: FullComponent,
-    children: [
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-      },
-      {
-        path: 'about',
-        loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
-      },
-      {
-        path: 'component',
-        loadChildren: () => import('./component/component.module').then(m => m.ComponentsModule)
-      }
-    ]
-  },
-  {
-    path: '**',
-    redirectTo: '/starter'
-  }
-];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
